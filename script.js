d409d5bc4e53e325508ff62c23a3565f5b7e6d98
Ilyass <iklassgames@gmail.com>
2025-08-13 17:50:35 +0100

    MODIFICATIONS:
    index.html
    - Changed the name of the page to Spirited Work
    - Added a description to the page
    - Commented the Break Duration section and the Note taking section
    
    styles.css
    - Changed the font
    - Maked the h1 bigger
    - Maked the hr thicker
    - Maked the buttons hoverable and blue
    - Commented the separator and the notes
    
    script.js
    - Created the whole timer system

--- script.js
+++ script.js
@@ -1,12 +1,50 @@
 // script.js file //
 
-let tenMinute = 2;
-let oneMinute = 5;
-let tenSeconde = 0;
-let oneSeconde = 0;
+const start = document.getElementById("start");
+const stop = document.getElementById("stop");
+const reset = document.getElementById("reset");
+const timer = document.getElementById("countDown");
 
-const countDown = document.getElementById("countDown");
+let timeLeft = 1500;
+let interval;
 
-let duration;
+function saveValue () {
+  timeLeft = parseInt((document.getElementById("workDuration").value) * 60, 10);
+  return timeLeft;
+}
 
-document.getElementById("countDown").innerHTML = tenMinute.toString()  + oneMinute.toString() + ":" + tenSeconde.toString() + oneSeconde.toString();
+const updateTimer = () => {
+  const minutes = Math.floor(timeLeft / 60);
+  const seconds = timeLeft % 60;
+
+  countDown.innerHTML = 
+  `${minutes.toString().padStart(2,"0")}
+  :
+  ${seconds.toString().padStart(2,"0")}`;
+};
+
+const startTimer = () => {
+  interval = setInterval(() => {
+    timeLeft--;
+    updateTimer();
+
+    if (timeLeft === 0) {
+      clearInterval(interval);
+      alert("Time's up!");
+      timeLeft = 1500;
+      updateTimer();
+    }
+  }, 1000)
+};
+
+const stopTimer = () => clearInterval(interval);
+
+const resetTimer = () => {
+  clearInterval(interval);
+  timeLeft = 1500;
+  updateTimer();
+};
+
+start.addEventListener("click", startTimer);
+stop.addEventListener("click", stopTimer);
+reset.addEventListener("click", resetTimer);
